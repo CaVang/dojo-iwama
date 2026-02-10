308f214b1ba674805453b7b8f68ec3eeef7b29f2
Khiet Nguyen <khiet@oddle.me>
2026-02-10 11:24:02 +0700

    debug

--- src/components/auth/AuthProvider.tsx
+++ src/components/auth/AuthProvider.tsx
@@ -100,9 +100,11 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
     const getSession = async () => {
       try {
+        console.log('getSession');
         const {
           data: { session },
         } = await supabase.auth.getSession();
+        console.log({session});
 
         setSession(session);
         setUser(session?.user ?? null);
